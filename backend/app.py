7f77375ba148b5292448c596967f7612bb53b195
Ryan Dias <ryan-diass@outlook.com>
2026-07-02 15:51:06 -0300

    Corrige imports

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,9 @@
 from flask  import Flask
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
+
 
 from backend.routes.transacoes_router import *
 from backend.routes.usuarios_router import *
